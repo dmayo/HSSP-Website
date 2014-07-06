c7b0bb1b85ae8d79591d3f484a2f30fa19aefd18
dmayo <dmayo2@mit.edu>
2014-07-06 14:53:23 -0400

    bug fiexes, site updates

--- psetchecker/index.php
+++ psetchecker/index.php
@@ -2,7 +2,7 @@
 require "checklogin.php";
 require "config/config.php";
 
-if(isset($_COOKIE["upload_user"])&&isset($_COOKIE["user_code"])&&checklogin ($db_server, $db_username, $db_password, $db_database, $_COOKIE["upload_user"], $_COOKIE["user_code"])==false) {
+if(!isset($_COOKIE["upload_user"])||!isset($_COOKIE["user_code"])||checklogin ($db_server, $db_username, $db_password, $db_database, $_COOKIE["upload_user"], $_COOKIE["user_code"])==false) {
     echo '<head><META HTTP-EQUIV="Pragma" CONTENT="no-cache"><META HTTP-EQUIV="Expires" CONTENT="-1"></head>';
 	echo '<meta http-equiv="refresh" content="0;url=login.php">';
 }
