5d9579837e6d16f3878ac50f756cd93ca25958df
dmayo <dmayo2@mit.edu>
2014-07-07 20:37:32 -0400

    posted all psets and notes for lecture 1

--- psetchecker/login/invite_user.php
+++
@@ -1,6 +0,0 @@
-<h3 style="text-align:left; padding-left:6px;">Invite Users</h3>	
-<div class="links">
-Please enter the email of the person who you wish to invite. <br />
-<form method="post" action="invite/invite.php" name="invite">
-<input type="text" name="email" /> <input type="submit" value="submit" onClick="submit1('configForm');"/>
-</div>
--- psetchecker/login/log/log.php
+++
@@ -1,8 +0,0 @@
-<?php
-function addLog($file,$time,$user,$event){
-	$message = $time . ';' . $user . ';' . $event . "\n";
-	$fh = fopen($file, 'a') or die("can't open file");
-	fwrite($fh, $message);
-	fclose($fh);
-}
-?>
